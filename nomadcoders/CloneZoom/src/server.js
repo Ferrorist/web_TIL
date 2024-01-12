deb4e5631c51c72cf2286882603e42090906ae42
parkdamin <ferrorist@naver.com>
2024-01-12 10:44:42 +0900

    nomadcoder: CloneZoom: First Recap

--- nomadcoders/CloneZoom/src/server.js
+++ nomadcoders/CloneZoom/src/server.js
@@ -6,10 +6,15 @@ const app = express();
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 
+// public 폴더를 유저에게 공개함.
 app.use("/public", express.static(__dirname + "/public"));
 
 // set router
 app.get("/", (req, res) => res.render("home"));
+
+// catchall url 설정
+// 유저가 어떤 url로 이동하던지 홈으로 redirect함.
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen); // use port number : 3000
