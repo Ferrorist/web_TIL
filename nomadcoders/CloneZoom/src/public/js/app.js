42cebb16aa3a9dc7e2eabf093f4fc05b62b4dee8
ferrorist <ferrorist@naver.com>
2024-01-18 07:30:34 +0900

    nomadcoder: CloneZoom: [Update] Chat

--- nomadcoders/CloneZoom/src/public/js/app.js
+++ nomadcoders/CloneZoom/src/public/js/app.js
@@ -1,8 +1,14 @@
 const messageList = document.querySelector("ul");
-const messageform = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageform = document.querySelector("#message");
 
 // 서버와 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 // socket이 open 되었다면, 이벤트 발생.
 socket.addEventListener("open", () => {
@@ -11,7 +17,10 @@ socket.addEventListener("open", () => {
 
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+    // console.log("New message: ", message.data);
     // message: MessageEvent
     // data, timestamp 등을 payload? 로 가짐.
 });
@@ -23,12 +32,22 @@ socket.addEventListener("close", () => {
 const handleSubmit = (event) => {
     event.preventDefault();
     const input = messageform.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
     input.value = "";
     // console.log(input.value);
 }
+
+const handleNickSubmit = (event) => {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
 messageform.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 
 // setTimeout(() => {
 //     socket.send("hello from the browser!");
 // }, 8000);
+
+
--- nomadcoders/CloneZoom/src/server.js
+++ nomadcoders/CloneZoom/src/server.js
@@ -29,12 +29,19 @@ const sockets = [];
 // 이 socket을 어딘가에 저장해야함.
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✔");
     socket.on("close", () => console.log("Disconnected from the Browser 💦"));
-    socket.on("message", message => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString('utf-8')));
-        // socket.send(message.toString('utf-8'));
-        // console.log(message.toString('utf8'));
+    socket.on("message", msg => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":     
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
 });
 
